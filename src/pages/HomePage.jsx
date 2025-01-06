32b84e9076e0c0013064dadc035381b45b140f6e
PranitAdgokar <136226664+PranitAdgokar@users.noreply.github.com>
2025-01-07 00:16:21 +0530

    add Sidebar component and implement chat store for user and message management in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import ComingSoon from '../components/coming-soon'
+import { useChatStore } from "../store/useChatStore";
+import { Sidebar } from "../components/Sidebar";
+
 
 const HomePage = () => {
+  const {selectedUser}=useChatStore();
+
   return (
-    <div className=" h-screen flex justify-center items-center text-center flex-col">
-      <div className="">
-          <h1 className="text-4xl font-bold">Welcome to Chatify</h1>
-          <p className="text-base-content/70">The best place to chat with your friends</p>
-    
-      </div>
-         <div className="mt-10">   
-          <ComingSoon />
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+            {!selectedUser? <NoChatSelected/>:<ChatContainer />}
+          </div>
         </div>
-
+      </div>
+      
     </div>
-  )
+  );
 }
-
-export default HomePage
---
+++ src/store/useChatStore.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUserLoding: false,
+  isMessagesLoading: false,
+
+  getUser: async () => {
+    set({ isUserLoding: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error("Failed to fetch users");
+    } finally {
+      set({ isUserLoding: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error("Failed to fetch messages");
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+}));
