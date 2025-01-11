4ec998a01e96bdba693d623b29b191dc96da3861
PranitAdgokar <136226664+PranitAdgokar@users.noreply.github.com>
2025-01-12 00:50:28 +0530

    enhance ChatContainer to format message timestamps and add message end reference; create utility function for time formatting

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -5,11 +5,14 @@ import ChatHeader from './ChatHeader';
 import ChatInput from './ChatInput';
 import MessageSkeleton from './MessageSkeleton';
 import { useAuthStore } from '../store/useAuthStore';
-
+import { useRef } from 'react'; 
+import { formatMessageTime } from '../lib/utils';
 
 const ChatContainer = () => {
   const {isMessagesLoading, getMessages, messages, selectedUser}=useChatStore();
 const {authUser}=useAuthStore();
+const messageEndRef = useRef(null);
+
   useEffect(()=>{
     getMessages(selectedUser._id);
   }
@@ -26,7 +29,10 @@ const {authUser}=useAuthStore();
       <ChatHeader />
     <div className="flex-1 overflow-y-auto p-4 space-y-4">
       {messages.map((message) => (
-        <div key={message._id} className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"} flex items-end space-x-2`}>
+        <div key={message._id}
+         className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"} `}
+         ref={messageEndRef}
+         >
           <div className="chat-image avatar">
             <div className="size-10 rounded-full border">
               <img src={message.senderId===authUser._id? authUser.profilePic || "/avatar.png" : selectedUser.profilePic || "/avatar.png"} alt="profile Pic" />
@@ -34,7 +40,7 @@ const {authUser}=useAuthStore();
           </div>
           <div className="chat-header mb-1">
             <time datetime="text-xs opacity-50 ml-1">
-              {message.createdAt}
+              {formatMessageTime (message.createdAt)}
             </time>
           </div>
           <div className="chat-bubble flex flex-col">
