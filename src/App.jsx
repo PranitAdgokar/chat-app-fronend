4ce2cd50e3eda89d7a8c6ac3503612af2aee3f8e
PranitAdgokar <136226664+PranitAdgokar@users.noreply.github.com>
2025-01-30 19:02:44 +0530

    refactor: improve code formatting and structure in App component for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,58 @@
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import SignUpPage from './pages/SignUpPage';
-import LoginPage from './pages/LoginPage';
-import SettingsPage from './pages/SettingsPage';
-import ProfilePage from './pages/ProfilePage';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
-import { useEffect } from 'react';
-import { Loader } from "lucide-react"
-import { Toaster } from 'react-hot-toast';
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
 
-const App =() => {
-  const {authUser,checkAuth, isCheckingAuth,onlineUsers } = useAuthStore()
-
-  console.log({onlineUsers});
-
-const {theme} = useThemeStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
-    checkAuth()
-  },[checkAuth]);
+    checkAuth();
+  }, [checkAuth]);
 
-
-
-  console.log({authUser});
   if (isCheckingAuth && !authUser) {
-    return <div className='flex justify-center items-center h-screen'>  
-      <Loader className="size-10 animate-spin" />
-    </div>
-  } ;
-
-
-
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
 
   return (
-   <div data-theme={theme}>
-    <Navbar />
-  
-    <Routes>
-      <Route path="/" element={authUser ? <HomePage />:<Navigate to ="/login"/>  } />
-      <Route path="/signup" element={!authUser ? <SignUpPage/> :<Navigate to="/"/>} />
-      <Route path="/login" element={!authUser ? <LoginPage />: <Navigate to="/"/>} />
-      <Route path="/settings" element={<SettingsPage />} />
-      <Route path="/profile" element={authUser ? <ProfilePage />:<Navigate to="/login"/>} />
+    <div data-theme={theme}>
+      <Navbar />
 
-    </Routes>
-    <Toaster />
-   
-   </div>
-  )
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 };
 export default App;
